e3909142c368c9d52fbffc9421666f66b2fa678c
syliang18 <syliang18@gmail.com>
2023-12-18 15:19:56 +0800

    Create test2.R

---
+++ test2.R
@@ -0,0 +1,7 @@
+##########################################
+# Add a new script called test.
+# Author: Suh-Yuen Liang
+# Date: 2023/12/18
+# Revision: 2023/12/18
+##########################################
+library(ggplot2)
