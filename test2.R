ef19cbaf08562c6b8b45244f7d3ae097100c79d1
syliang18 <syliang18@gmail.com>
2023-12-18 15:24:22 +0800

    Make a line plot with ggplot

--- test2.R
+++ test2.R
@@ -5,9 +5,13 @@
 # Revision: 2023/12/18
 ##########################################
 library(ggplot2)
-
+library(dplyr)
 # show sessioninfo
 sessionInfo()
 
 # show dataset 
 head(datasets::pressure)
+
+# show line plot
+ggplot(pressure, aes(x=temperature, y=pressure))+
+  geom_line()
