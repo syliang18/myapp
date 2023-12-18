ae2aa015912ade92401362fc418df79e8463c767
syliang18 <syliang18@gmail.com>
2023-12-18 17:01:47 +0800

    Add a boxplot with labels.

--- test2.R
+++ test2.R
@@ -20,3 +20,16 @@ ggplot(pressure, aes(x=temperature, y=pressure))+
   ggtitle('This is a line plot')+
   theme(title=element_text(size=20, face="bold", hjust=0.5),
   axis.title = element_text(size=10,  hjust=0.5))
+
+# show bar plot
+head(datasets::CO2)
+
+ggplot(CO2, aes(x=interaction(Type,Plant), y=conc, color=Treatment))+
+  geom_boxplot() +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle=90, hjust=1))
+
+
+CO2%>%
+  group_by(Type, Plant, Treatment) %>%
+  summarise(mean=mean(conc))
