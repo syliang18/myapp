633bce40c884e201163806c41ba5d10c0f83046e
syliang18 <syliang18@gmail.com>
2023-12-18 15:01:32 +0800

    Create test.R

---
+++ test.R
@@ -0,0 +1,12 @@
+##########################################
+# Add a new script called test.
+# Author: Suh-Yuen Liang
+# Date: 2023/12/18
+# Revision: 2023/12/18
+##########################################
+library(dplyr)
+
+getwd()
+
+# show session.info
+sessionInfo()
